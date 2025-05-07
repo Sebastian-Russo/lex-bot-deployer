3678321e7d550e1bb5179d1fbb955756503eba09
Sebastian Russo <russo.sebastian@gmail.com>
2025-05-07 15:55:39 -0400

    Updated associate lex bot contruct. Lex bots were NOT being deployed before. Successfully deployed AgentBusyBot - true firsttime.

--- stacks/constructs/associate_lex_bot.py
+++ stacks/constructs/associate_lex_bot.py
@@ -4,6 +4,7 @@ from aws_cdk import Aws
 from constructs import Construct
 from dataclasses import dataclass
 
+# Keep the props class for backward compatibility
 @dataclass
 class AssociateLexBotProps:
     """
@@ -16,9 +17,17 @@ class AssociateLexBot(connect.CfnIntegrationAssociation):
     """
     Associate a Lex alias with a Connect instance
     """
-    def __init__(self, scope: Construct, id: str, props: AssociateLexBotProps):
-        connect_instance_arn = props.connect_instance_arn
-        alias = props.alias
+    def __init__(self, scope: Construct, id: str, *,
+                 connect_instance_arn: str = None,
+                 alias: lex.CfnBotAlias = None,
+                 props: AssociateLexBotProps = None):
+        # Support both styles - props object or individual parameters
+        if props:
+            connect_instance_arn = props.connect_instance_arn
+            alias = props.alias
+
+        if not connect_instance_arn or not alias:
+            raise ValueError("Either props or both connect_instance_arn and alias must be provided")
 
         super().__init__(
             scope,
