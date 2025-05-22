6e1078cdd719a1e48862dd7c9dadd07412fd4ab9
Ivan Bliskavka <ibliskavka@live.com>
2025-05-22 10:37:31 -0400

    chore: change name to infrastructure

--- app.py
+++ app.py
@@ -10,12 +10,12 @@ prefix = 'lex-deploy-demo-py'
 LexStack(app, 'LexPy',
     prefix=prefix,
     env=Environment(
-        account='308665918648',
+        account='857240696749',
         region='us-east-1'
     ),
-    connect_instance_arn='arn:aws:connect:us-east-1:308665918648:instance/ee5e36fa-330b-4a91-995d-c218d84b8fea',
-    city_hall_queue_arn='arn:aws:connect:us-east-1:308665918648:instance/ee5e36fa-330b-4a91-995d-c218d84b8fea/queue/dfce6ff7-c5be-4000-8113-8c4a99c72389',
-    city_manager_flow_arn='arn:aws:connect:us-east-1:308665918648:instance/ee5e36fa-330b-4a91-995d-c218d84b8fea/contact-flow/4a209a43-7401-43f0-ab14-adcc6bb8b781'
+    connect_instance_arn='arn:aws:connect:us-east-1:857240696749:instance/ca082b7f-74b2-4935-8985-460415c56d14',
+    city_hall_queue_arn='arn:aws:connect:us-east-1:857240696749:instance/ca082b7f-74b2-4935-8985-460415c56d14/queue/5787a8fc-42cf-40b6-8180-9cb2150446b9',
+    city_manager_flow_arn='arn:aws:connect:us-east-1:857240696749:instance/ca082b7f-74b2-4935-8985-460415c56d14/contact-flow/c07ddbba-4788-4853-90be-8eea2fa57f37'
 )
 
 app.synth()
