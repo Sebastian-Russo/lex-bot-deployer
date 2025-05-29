c3cbb1af5f298d16e5e336677ce25d4f8e15f37b
Sebastian Russo <russo.sebastian@gmail.com>
2025-05-29 11:32:09 -0400

    Created basic connect flows for each bot. Added ARNs to app. Added json flows to each bot folder.

--- app.py
+++ app.py
@@ -17,11 +17,11 @@ LexStack(
     city_manager_flow_arn='arn:aws:connect:us-east-1:308665918648:instance/ee5e36fa-330b-4a91-995d-c218d84b8fea/contact-flow/4a209a43-7401-43f0-ab14-adcc6bb8b781',
     reprint_1099_flow_arn='arn:aws:connect:us-east-1:308665918648:instance/ee5e36fa-330b-4a91-995d-c218d84b8fea/contact-flow/a08c07f3-1af8-4306-8ce6-5b5a252e7dc8',
     pamphlet_flow_arn='arn:aws:connect:us-east-1:308665918648:instance/ee5e36fa-330b-4a91-995d-c218d84b8fea/contact-flow/4d3bf4e8-f1ff-4808-9de9-cfd2206984c4',
-    medicare_enrollment_flow_arn='',
-    medicare_card_replacement_flow_arn='',
-    ssn_replacement_form_flow_arn='',
-    change_of_address_flow_arn='',
-    benefit_payment_flow_arn='',
+    medicare_enrollment_flow_arn='arn:aws:connect:us-east-1:308665918648:instance/ee5e36fa-330b-4a91-995d-c218d84b8fea/contact-flow/7136ccab-51a1-438e-921f-49244f341ba6',
+    medicare_card_replacement_flow_arn='arn:aws:connect:us-east-1:308665918648:instance/ee5e36fa-330b-4a91-995d-c218d84b8fea/contact-flow/cd58671d-abc3-4625-ba27-b4e98fde2fcf',
+    ssn_replacement_form_flow_arn='arn:aws:connect:us-east-1:308665918648:instance/ee5e36fa-330b-4a91-995d-c218d84b8fea/contact-flow/7aff72cf-ba07-43d3-8ee7-8516349847d3',
+    change_of_address_flow_arn='arn:aws:connect:us-east-1:308665918648:instance/ee5e36fa-330b-4a91-995d-c218d84b8fea/contact-flow/e66bfb50-d271-49f2-8ba3-474792076aea',
+    benefit_payment_flow_arn='arn:aws:connect:us-east-1:308665918648:instance/ee5e36fa-330b-4a91-995d-c218d84b8fea/contact-flow/67b97347-2363-4a26-8e98-a8b40459661a',
 )
 
 app.synth()
